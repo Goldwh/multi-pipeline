96a886ab4282c24d6a9852b74015bb8e6df9470f
Goldwh <794918503@qq.com>
2020-02-12 09:04:16 +0800

    add main.c file

---
+++ main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int i = 10;
+    printf("Hello world\n");
+    printf("thank you\n");
+    while(i--)
+	{
+	    printf("*");	
+	    printf("i = %d",i);
+	    printf("*\n");
+	}
+    exit(0);
+}
